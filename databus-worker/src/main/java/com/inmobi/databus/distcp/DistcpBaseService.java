a4e474e39f674202bdc77f59b2bc633150722fc9
rohit.kochar <rohit.kochar@inmobi.com>
2013-01-14 11:53:52 +0530

    checking whether a path is invalid or yet to be published

--- databus-worker/src/main/java/com/inmobi/databus/distcp/DistcpBaseService.java
+++ databus-worker/src/main/java/com/inmobi/databus/distcp/DistcpBaseService.java
@@ -20,15 +20,11 @@ import java.net.URI;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringTokenizer;
 
-import com.inmobi.databus.AbstractService;
-import com.inmobi.databus.Cluster;
-import com.inmobi.databus.DatabusConfig;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -41,12 +37,18 @@ import org.apache.hadoop.tools.DistCp;
 import org.apache.hadoop.tools.DistCpConstants;
 import org.apache.hadoop.tools.DistCpOptions;
 
+import com.inmobi.databus.AbstractService;
+import com.inmobi.databus.Cluster;
+import com.inmobi.databus.DatabusConfig;
+import com.inmobi.databus.utils.CalendarHelper;
+
 public abstract class DistcpBaseService extends AbstractService {
 
   private final Cluster srcCluster;
   private final Cluster destCluster;
   private final FileSystem srcFs;
   private final FileSystem destFs;
+
   protected static final int DISTCP_SUCCESS = DistCpConstants.SUCCESS;
 
   protected static final Log LOG = LogFactory.getLog(DistcpBaseService
@@ -187,7 +189,8 @@ public abstract class DistcpBaseService extends AbstractService {
    * @return
    */
   protected Path getDistCPInputFile(Map<Path, FileSystem> consumePaths,
-                                    Path tmp) throws IOException {
+ Path tmp)
+          throws Exception {
     Path input = getInputPath();
     if (!srcFs.exists(input))
       return null;
@@ -195,6 +198,7 @@ public abstract class DistcpBaseService extends AbstractService {
     FileStatus[] fileList = srcFs.listStatus(input);
     if (fileList != null) {
       Set<String> minFilesSet = new HashSet<String>();
+      Set<Path> yetToBeMovedPaths = new HashSet<Path>();
       /* inputPath could have multiple files due to backlog
       * read all and create a Input file for DISTCP with valid paths on
       * destinationCluster as an optimization so that distcp doesn't pull
@@ -212,10 +216,13 @@ public abstract class DistcpBaseService extends AbstractService {
             consumeFilePath + "]");
         //read all valid minute files path in each consumePath
         readConsumePath(srcFs, consumeFilePath,
-            minFilesSet);
+ minFilesSet, yetToBeMovedPaths);
       }
       // removing those path which have same file name
       filterMinFilePaths(minFilesSet);
+      // writing yet to be moved file on the source cluster
+      writeYetToBeMovedFile(srcCluster.getTmpPath(), yetToBeMovedPaths);
+
       Path tmpPath = createInputFileForDISCTP(destFs, srcCluster.getName(),
           tmp, minFilesSet);
       return getFinalPathForDistCP(tmpPath, consumePaths);
@@ -230,7 +237,7 @@ public abstract class DistcpBaseService extends AbstractService {
    * read each consumePath and add only valid paths to minFilesSet
    */
   private void readConsumePath(FileSystem fs,Path consumePath,
-                               Set<String>  minFilesSet)  throws IOException{
+      Set<String> minFilesSet, Set<Path> yetToBeMovedPaths) throws IOException {
     BufferedReader reader = null;
     try {
       FSDataInputStream fsDataInputStream = fs.open(consumePath);
@@ -241,22 +248,30 @@ public abstract class DistcpBaseService extends AbstractService {
         minFileName = reader.readLine();
         if (minFileName != null) {
           /*
-          * To avoid data-loss in all services we publish the paths to
-          * consumers directory first before publishing on HDFS for
-          * finalConsumption. In a distributed transaction failure it's
-          * possible that some of these paths do not exist. Do isExistence
-          * check before adding them as DISTCP input otherwise DISTCP
-          * jobs can fail continously thereby blocking Merge/Mirror
-          * stream to run further
-          */
+           * To avoid data-loss in all services we publish the paths to
+           * consumers directory first before publishing on HDFS for
+           * finalConsumption. In a distributed transaction failure it's
+           * possible that some of these paths do not exist. Do isExistence
+           * check before adding them as DISTCP input otherwise DISTCP jobs can
+           * fail continuously thereby blocking Merge/Mirror stream to run
+           * further
+           */
           Path p = new Path(minFileName);
           if (fs.exists(p)) {
             LOG.info("Adding sourceFile [" + minFileName + "] to distcp " +
                 "FinalList");
             minFilesSet.add(minFileName.trim());
           } else {
+            // if a path doesn't exist than it there can be 2 cases:
+            // 1) previous service failed after writing consumer paths
+            // 2) previous service is still working and its in between writing
+            // consumer paths and publishing data
+            if (isMoveFailed(fs, p)) {
             LOG.info("Skipping [" + minFileName + "] to pull as it's an " +
                 "INVALID PATH");
+            } else {
+              yetToBeMovedPaths.add(p);
+            }
           }
         }
       } while (minFileName != null);
@@ -264,6 +279,111 @@ public abstract class DistcpBaseService extends AbstractService {
       if (reader != null)
         reader.close();
     }
+  }
+
+  /*
+   * Method to check whether movement of file from temp location to Path p has
+   * failed or is under process
+   */
+  private boolean isMoveFailed(FileSystem fs, Path p) throws IOException {
+    Path nextPath = getNextMinuteDirectory(p);
+    if (nextPath == null) {
+      LOG.error("Path[" + p + "] is invalid");
+      return true;// so that invalid path can be skipped
+    }
+    if (fs.exists(nextPath))
+      return true;
+    return false;
+  }
+
+  // public static void main(String args[]) {
+  // Path root = new Path("hdfs://ruby-nn.grid.ua2.inmobi.com:8020/databus/");
+  // Path p = new Path(
+  // "hdfs://ruby-nn.grid.ua2.inmobi.com:8020/databus/streams/adroit_feedback_delta/2013/01/10/12/33");
+  // String[] tmp = p.toString().substring(root.toString().length() +
+  // 1).split("/");
+  // Path tmp1 = new Path(root,tmp[0]);
+  // Path tmp2 = new Path(tmp1,tmp[1]);
+  // System.out.println(tmp2);
+  // Date pathDate = CalendarHelper.getDateFromStreamDir(tmp2, p);
+  // System.out.println(pathDate);
+  // Calendar calendar = Calendar.getInstance();
+  // calendar.setTime(pathDate);
+  // calendar.add(Calendar.MINUTE, 1);
+  // Date nextdate = calendar.getTime();
+  // String suffix = Cluster.getDateAsYYYYMMDDHHMNPath(nextdate);
+  // System.out.println(tmp2 + File.separator + suffix);
+  // }
+
+  /*
+   * Helper function to find next minute directory given a path eg: Path p =
+   * "hdfs://ruby-nn.grid.ua2.inmobi.com:8020/databus/streams/adroit_feedback_delta/2013/01/10/12/33"
+   */
+  private Path getNextMinuteDirectory(Path p) {
+    String root = srcCluster.getRootDir();
+    // eg: root = "hdfs://ruby-nn.grid.ua2.inmobi.com:8020/databus/"
+    String path = p.toString();
+    // if it is a valid path than this condition should be true
+    if (path.length() >= root.length() + 1) {
+      // tmpPath will have value of form
+      // "streams/adroit_feedback_delta/2013/01/10/12/33"
+      String tmpPath = path.substring(root.length() + 1);
+      String tmp[] = tmpPath.split("/");
+      if (tmp.length >= 2) {
+        // tmp[0] value should either be streams or steam_local and tmp[1] value
+        // will be name of topic
+        Path pathTillStream = new Path(new Path(root), tmp[0]);
+        Path pathTillTopic = new Path(pathTillStream, tmp[1]);
+        // pathTillTopic value will be of form
+        // hdfs://ruby-nn.grid.ua2.inmobi.com:8020/databus/streams/adroit_feedback_delta/
+        Date pathDate = CalendarHelper.getDateFromStreamDir(pathTillTopic, p);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(pathDate);
+        calendar.add(Calendar.MINUTE, 1);
+        Date nextdate = calendar.getTime();
+        String suffix = Cluster.getDateAsYYYYMMDDHHMNPath(nextdate);
+        Path nextPath = new Path(pathTillTopic, suffix);
+        return nextPath;
+      }
+    }
+
+    return null;
+  }
+
+  private void writeYetToBeMovedFile(Path tmp,
+      Set<Path> yetToBeMovedPaths)
+      throws Exception
+ {
+    String tmpPath = "yet_to_be_moved_src" + srcCluster.getName()
+        + "_to_destn_" + destCluster.getName() + "_"
+        + System.currentTimeMillis();
+    Path temporary = new Path(tmp, tmpPath);
+    Map<Path, Path> ytmCommitPaths = new HashMap<Path, Path>();
+    FSDataOutputStream out = null;
+    try {
+      out = srcFs.create(temporary);
+      for (Path p : yetToBeMovedPaths) {
+        LOG.debug("Writing yet To Be Moved Path [" + p + "]");
+        out.writeBytes(p.toString());
+        out.writeBytes("\n");
+      }
+    } catch (IOException e) {
+      LOG.error("Cannot create yet_to_b_emoved file in[" + temporary + "]", e);
+    } finally {
+      if (out != null) {
+        try {
+          out.close();
+        } catch (IOException e) {
+          LOG.error("Cannot close the file [" + temporary + "]", e);
+        }
+      }
+    }
+
+    Path finalYetToBeMoved = new Path(getInputPath(),
+        ("yet_to_be_moved_" + System.currentTimeMillis()));
+    ytmCommitPaths.put(temporary, finalYetToBeMoved);
+    doRemoteCommit(ytmCommitPaths);
+
   }
 
   /*
@@ -284,6 +404,20 @@ public abstract class DistcpBaseService extends AbstractService {
     }
   }
 
+  private void doRemoteCommit(Map<Path, Path> ytmCommitPaths) throws Exception {
+    LOG.info("Committing " + ytmCommitPaths.size() + " paths.");
+
+    for (Map.Entry<Path, Path> entry : ytmCommitPaths.entrySet()) {
+      LOG.info("Renaming " + entry.getKey() + " to " + entry.getValue());
+      srcFs.mkdirs(entry.getValue().getParent());
+      if (srcFs.rename(entry.getKey(), entry.getValue()) == false) {
+        LOG.warn("Rename failed, aborting transaction COMMIT to avoid "
+            + "dataloss.");
+        throw new Exception("Abort transaction Commit. Rename failed from ["
+            + entry.getKey() + "] to [" + entry.getValue() + "]");
+      }
+    }
+  }
   /*
    * Helper method for getDistCPInputFile
    * if none of the paths are VALID then it does not create an empty file on
