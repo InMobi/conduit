234154d03e021f947555b183554ae71c171cd39e
rohitkochar <rohit.kochar@inmobi.com>
2013-07-08 17:02:27 +0530

    renaming config key of table name

--- databus-audit/src/main/java/com/inmobi/databus/audit/util/AuditDBConstants.java
+++ databus-audit/src/main/java/com/inmobi/databus/audit/util/AuditDBConstants.java
@@ -6,7 +6,7 @@ public interface AuditDBConstants {
   public static final String DB_URL = "db.url";
   public static final String DB_USERNAME = "db.username";
   public static final String DB_PASSWORD = "db.password";
-  public static final String MASTER_TABLE_NAME = "feeder.table.master";
+  public static final String MASTER_TABLE_NAME = "audit.table.master";
   public static final String TIMESTAMP = "TIMEINTERVAL";
   public static final String SENT = "SENT";
   public static final String GANGLIA_HOST = "feeder.ganglia.host";
