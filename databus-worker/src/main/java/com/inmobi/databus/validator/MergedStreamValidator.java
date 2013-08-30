a863879bd47f465f61c4b95fb3651b07e6bf6ca8
rohitkochar <rohit.kochar@inmobi.com>
2013-08-30 15:17:42 +0530

    adding missing validator files

---
+++ databus-worker/src/main/java/com/inmobi/databus/validator/MergedStreamValidator.java
@@ -0,0 +1,244 @@
+package com.inmobi.databus.validator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import com.inmobi.databus.Cluster;
+import com.inmobi.databus.DatabusConfig;
+import com.inmobi.databus.distcp.MergedStreamService;
+import com.inmobi.databus.utils.ParallelRecursiveListing;
+
+public class MergedStreamValidator extends AbstractStreamValidator {
+  private static final Log LOG = LogFactory.getLog(MergedStreamValidator.class);
+  private DatabusConfig databusConfig = null;
+  private String streamName = null;
+  private boolean fix = false;
+  List<Path> duplicateFiles = new ArrayList<Path>();
+  List<Path> holesInMerge = new ArrayList<Path>();
+
+  List<Path> holesInLocal = new ArrayList<Path>();
+
+  List<Cluster> srcClusterList = new ArrayList<Cluster>();
+  Cluster mergeCluster = null;
+  private Date startTime = null;
+  private Date stopTime = null;
+  private int numThreads;
+
+  public MergedStreamValidator(DatabusConfig databusConfig, String streamName,
+      String clusterName, boolean fix, Date startTime, Date stopTime,
+      int numThreads) {
+    this.databusConfig = databusConfig;
+    this.streamName = streamName;
+    this.fix = fix;
+    for (Cluster cluster : databusConfig.getClusters().values()) {
+      if (cluster.getSourceStreams().contains(streamName)) {
+        srcClusterList.add(cluster);
+      }
+    }
+    mergeCluster = databusConfig.getClusters().get(clusterName);
+    this.startTime = startTime;
+    this.stopTime = stopTime;
+    this.numThreads = numThreads;
+  }
+
+  public void execute() throws Exception {
+    // check whether the given start time is beyond retention period for all 
+    // source clusters
+    for (Cluster srcCluster : srcClusterList) {
+      validateStartTime(srcCluster);
+    }
+    // perform recursive listing on merge cluster
+    Map<String, FileStatus> mergeStreamFileListing = new TreeMap<String, FileStatus>();
+    Path mergePath = new Path(mergeCluster.getFinalDestDirRoot(), streamName);
+    FileSystem mergedFs = FileSystem.get(mergeCluster.getHadoopConf());
+    ParallelRecursiveListing mergeParallelListing = 
+        new ParallelRecursiveListing(numThreads, null, null);
+    List<FileStatus> mergeStreamFileStatuses = 
+        mergeParallelListing.getListing(mergePath, mergedFs, true);
+
+    //find duplicates on merged cluster
+    findDuplicates(mergeStreamFileStatuses, mergeStreamFileListing);
+    // find holes on merge cluster
+    holesInMerge.addAll(findHoles(mergeStreamFileStatuses, mergePath, mergedFs));
+    if (!holesInMerge.isEmpty()) {
+      LOG.info("holes in [ " + mergeCluster.getName() + " ] " + holesInMerge);
+    }  
+
+    Map<String, FileStatus> localStreamFileListing = new TreeMap<String, FileStatus>();
+    // perform recursive listing on each source cluster
+    for (Cluster srcCluster : srcClusterList) {
+      Path localStreamPath = new Path(srcCluster.getLocalFinalDestDirRoot(),
+          streamName);
+      FileSystem localFs = FileSystem.get(srcCluster.getHadoopConf());
+      Path startPath = new Path(localStreamPath,
+          Cluster.getDateAsYYYYMMDDHHMNPath(startTime));
+      Path endPath = new Path(localStreamPath,
+          Cluster.getDateAsYYYYMMDDHHMNPath(stopTime));
+
+      ParallelRecursiveListing localParallelListing =
+          new ParallelRecursiveListing(numThreads, startPath, endPath);
+      List<FileStatus> localStreamFileStatuses =
+          localParallelListing.getListing(localStreamPath, localFs, true);
+
+      //find holes on source cluster
+      List<Path> holesInLocalCluster = findHoles(localStreamFileStatuses,
+          localStreamPath, localFs);
+      if (!holesInLocalCluster.isEmpty()) {
+        holesInLocal.addAll(holesInLocalCluster);
+        LOG.info("holes in [ " + srcCluster.getName() + " ] "
+            + holesInLocalCluster);
+      }
+      convertListToMap(localStreamFileStatuses, localStreamFileListing);
+      //find missing paths on merge cluster
+      findMissingPaths(localStreamFileListing, mergeStreamFileListing);
+
+      if (fix && !missingPaths.isEmpty()) {
+        copyMissingPaths(srcCluster);
+        // clear missing paths list
+        missingPaths.clear();
+      }
+      // clear the localStream file list map
+      localStreamFileListing.clear();
+    }
+    if (fix && !holesInMerge.isEmpty()) {
+      fixHoles(holesInMerge, mergedFs);
+    }
+  }
+
+  protected void convertListToMap(List<FileStatus> listOfFileStatuses,
+      Map<String, FileStatus> streamListingMap) {
+    for (FileStatus file : listOfFileStatuses) {
+      if (!file.isDir()) {
+        streamListingMap.put(file.getPath().getName(), file);
+      }
+    }
+  }
+
+  protected void findDuplicates(List<FileStatus> listOfFileStatuses,
+      Map<String, FileStatus> streamListingMap) {
+    String fileName;
+    for (FileStatus fileStatus : listOfFileStatuses) {
+      if (fileStatus.isDir()) {
+        continue;
+      }
+      fileName = fileStatus.getPath().getName();
+      if (streamListingMap.containsKey(fileName)) {
+        Path duplicatePath;
+        Path existingPath = streamListingMap.get(fileName).getPath();
+        Path currentPath = fileStatus.getPath();
+        if (existingPath.compareTo(currentPath) < 0) {
+          duplicatePath = currentPath;
+        } else {
+          duplicatePath = existingPath;
+          // insert this entry into the map because this file was created first
+          streamListingMap.put(fileName, fileStatus);
+        }
+        duplicateFiles.add(duplicatePath);
+        LOG.info("Duplicate file " + duplicatePath);
+      } else {
+        streamListingMap.put(fileName, fileStatus);
+      }
+    }
+  }
+
+  protected void findMissingPaths(Map<String, FileStatus> srcListingMap,
+      Map<String, FileStatus> destListingMap) {
+    String fileName = null;
+    for (Map.Entry<String, FileStatus> srcEntry : srcListingMap.entrySet()) {
+      fileName = srcEntry.getKey();
+      if (!destListingMap.containsKey(fileName)) {
+        LOG.info("Missing path " + srcEntry.getValue().getPath());
+        missingPaths.put(getFinalDestinationPath(srcEntry.getValue()),
+            srcEntry.getValue());
+      }
+    }
+  }
+
+  private void validateStartTime(Cluster srcCluster) {
+    int retentionHours = databusConfig.getSourceStreams().
+        get(streamName).getRetentionInHours(srcCluster.getName());
+    Calendar cal = Calendar.getInstance();
+    cal.add(Calendar.HOUR_OF_DAY, -retentionHours);
+    if (cal.getTime().after(startTime)) {
+      throw new IllegalArgumentException("Provided start time [" + startTime.toString()
+          + "] is beyond the retention period [" + cal.getTime().toString() 
+          + "] for cluster [" + srcCluster.getName() + "]");
+    }
+  }
+
+  private void copyMissingPaths(Cluster srcCluster)
+      throws Exception {
+    // create an instance of MergedStreamFixService and invoke execute
+    Set<String> streamsToProcess = new HashSet<String>();
+    streamsToProcess.add(streamName);
+    MergedStreamFixService mergeFixService = new MergedStreamFixService(
+        databusConfig, srcCluster, mergeCluster, streamsToProcess);
+    // copy the missing paths through distcp and commit the copied paths
+    mergeFixService.execute();
+  }
+
+  @Override
+  protected String getFinalDestinationPath(FileStatus srcPath) {
+    if (srcPath.isDir())
+      return null;
+    else
+      return File.separator + srcPath.getPath().getName();
+
+  }
+
+  public List<Path> getDuplicateFiles() {
+    return duplicateFiles;
+  }
+
+  public List<Path> getHolesInLocal() {
+    return holesInLocal;
+  }
+
+  public List<Path> getHolesInMerge() {
+    return holesInMerge;
+  }
+
+  class MergedStreamFixService extends MergedStreamService {
+    public MergedStreamFixService(DatabusConfig config, Cluster srcCluster,
+        Cluster destinationCluster, Set<String> streamsToProcess)
+            throws Exception {
+      super(config, srcCluster, destinationCluster, null, null, streamsToProcess);
+    }
+
+    @Override
+    protected Path getDistCpTargetPath() {
+      return new Path(getDestCluster().getTmpPath(),
+          "distcp_mergedStream_fix_" + getSrcCluster().getName() + "_"
+              + getDestCluster().getName() + "_"
+              + getServiceName(streamsToProcess)).makeQualified(getDestFs());
+    }
+
+    @Override
+    protected Map<String, FileStatus> getDistCPInputFile() throws Exception {
+      return missingPaths;
+    }
+
+    @Override
+    public void execute() throws Exception {
+      super.execute();
+    }
+
+    @Override
+    protected void finalizeCheckPoints() {
+      LOG.info("Skipping update of checkpoints in Merge Stream Fix Service run");
+    }
+  }
+}
---
+++ databus-worker/src/main/java/com/inmobi/databus/validator/StreamsValidator.java
@@ -0,0 +1,128 @@
+package com.inmobi.databus.validator;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.inmobi.databus.Cluster;
+import com.inmobi.databus.DatabusConfig;
+
+public class StreamsValidator {
+  private static final Log LOG = LogFactory.getLog(StreamsValidator.class);
+  private DatabusConfig databusConfig = null;
+  private Set<String> streams = new HashSet<String>();
+  private Set<String> modes = new HashSet<String>();
+  private Set<String> clusters = new HashSet<String>();
+  private Date startTime;
+  private Date stopTime;
+  private int numThreads;
+
+  public StreamsValidator(DatabusConfig databusConfig, String streamNames, 
+      String modeNames, String clusterNames, Date startTime, Date stopTime,
+      int numThreads) {
+    this.databusConfig = databusConfig;
+
+    if (streamNames == null || streamNames.isEmpty()) {
+      // add ALL streams from databus config
+      for (String stream : databusConfig.getSourceStreams().keySet()) {
+        streams.add(stream);
+      }
+    } else {
+      for (String stream : streamNames.split(",")) {
+        streams.add(stream);
+      }
+    }
+
+    if (modeNames == null || modeNames.isEmpty()) {
+      // add ALL modes for stream validation
+      modes.add("local");
+      modes.add("merge");
+      modes.add("mirror");
+    } else {
+      for (String mode : modeNames.split(",")) {
+        modes.add(mode.toLowerCase());
+      }
+    }
+
+    if (clusterNames == null || clusterNames.isEmpty()) {
+      //add ALL clusters from databus config
+      for (String cluster : databusConfig.getClusters().keySet()) {
+        clusters.add(cluster);
+      }
+    } else {
+      for (String cluster : clusterNames.split(",")) {
+        clusters.add(cluster);
+      }
+    }
+    this.startTime = startTime;
+    this.stopTime = stopTime;
+    this.numThreads = numThreads;
+  }
+
+  public void validateStreams(boolean fix) throws Exception {
+    for (String stream : streams) {
+      // run the stream validation in the following order of modes:
+      // LOCAL, MERGE, MIRROR
+      if (modes.contains("local")) {
+        validateLocalStream(stream, fix);
+      }
+      if (modes.contains("merge")) {
+        validateMergeStream(stream, fix);
+      }
+      if (modes.contains("mirror")) {
+        validateMirrorStream(stream, fix);
+      }
+    }
+  }
+
+  private void validateLocalStream(String stream, boolean fix) throws Exception {
+    // for each cluster, check whether stream runs in local mode
+    for (String clusterName: clusters) {
+      Cluster cluster = databusConfig.getClusters().get(clusterName);
+      if (!cluster.getSourceStreams().contains(stream)) {
+        System.out.println("ERROR: The stream [" + stream +
+            "] is not running in [LOCAL] mode on cluster [" + clusterName + "]");
+        continue;
+      }
+    }
+      
+      // TODO: perform local stream validation
+  }
+
+  private void validateMergeStream(String stream, boolean fix) throws Exception {
+    // for each cluster, check whether stream runs in merge mode
+    for (String clusterName: clusters) {
+      Cluster cluster = databusConfig.getClusters().get(clusterName);
+      if (!cluster.getPrimaryDestinationStreams().contains(stream)) {
+        System.out.println("ERROR: The stream [" + stream +
+            "] is not running in [MERGE] mode on cluster [" + clusterName + "]");
+        continue;
+      }
+      
+      MergedStreamValidator mergeValidator = new MergedStreamValidator(
+          databusConfig, stream, clusterName, fix, startTime, stopTime,
+          numThreads);
+      mergeValidator.execute();
+    }
+  }
+
+  private void validateMirrorStream(String stream, boolean fix) throws Exception {
+    // for each cluster, check whether stream runs in mirror mode
+    for (String clusterName: clusters) {
+      Cluster cluster = databusConfig.getClusters().get(clusterName);
+      if (!cluster.getMirroredStreams().contains(stream)) {
+        System.out.println("ERROR: The stream [" + stream +
+            "] is not running in [MIRROR] mode on cluster [" + clusterName + "]");
+        continue;
+      }
+      // add start time and stop time
+      MirrorStreamValidator mirrorValidator = new MirrorStreamValidator(
+          databusConfig, stream, clusterName, fix, startTime, stopTime,
+          numThreads);
+      mirrorValidator.execute();
+    }
+  }
+}
