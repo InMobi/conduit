5bf9dc10df3e8a33e573679cd29cd3ad0802bf14
rohitkochar <rohit.kochar@inmobi.com>
2013-09-30 22:52:31 +0530

    changing text in sample conf

--- databus-visualization/conf/setenv.sh
+++ databus-visualization/conf/setenv.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 #hadoop-home-path is path till hadoop core jar
-export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=<hadoop-home-path>/*.jar"
+export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=<path to hadoop core jar>
