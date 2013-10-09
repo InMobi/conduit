1422ca5d3b7d5e1419b533e861079f65590ab71b
samar <samar.kumar@inmobi.com>
2013-10-09 13:37:42 +0530

    remove author,line wrapping

--- databus-core/src/main/java/com/inmobi/conduit/metrics/ConduitMetrics.java
+++ databus-core/src/main/java/com/inmobi/conduit/metrics/ConduitMetrics.java
@@ -6,9 +6,9 @@ import info.ganglia.gmetric4j.gmetric.GMetric.UDPAddressingMode;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.configuration.Configuration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -18,18 +18,20 @@ import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.ScheduledReporter;
 import com.codahale.metrics.ganglia.GangliaReporter;
+
 /**
- * Metrics manager class. Will use codahale metrics as the implementation. Contains functions to register Gauges/Counters and start/stop reporters
- * @author samar.kumar
- *
+ * Metrics manager class. Will use codahale metrics as the implementation.
+ * Contains functions to register Gauges/Counters and start/stop reporters
+ * 
+ * 
  */
 public class ConduitMetrics {
 
 	private static final Log LOG = LogFactory.getLog(ConduitMetrics.class);
 
-	private final static MetricRegistry registry;
+	private final static MetricRegistry registry= new MetricRegistry();;
 
-	private final static String GANGLIA = "ganlia";
+	private final static String GANGLIA = "ganglia";
 	private final static String CONSOLE = "console";
 
 	private final static String GANGLIA_SERVERNAME = "ganglia.serverName";
@@ -37,25 +39,29 @@ public class ConduitMetrics {
 
 	private final static Map<String, ScheduledReporter> reporterMap = new HashMap<String, ScheduledReporter>();
 
-	static {
-		registry = new MetricRegistry();
-	}
 
 	/**
 	 * Will create reporters based on config
+	 * 
 	 * @param config
 	 * @throws IOException
 	 */
-	public static void init(Configuration config) throws IOException {
+	public static void init(Properties config) throws IOException {
 
-		if (config.getBoolean(GANGLIA, false)) {
-			final GMetric ganglia = new GMetric(config.getString(GANGLIA_SERVERNAME), config.getInt(GANGLIA_PORT), UDPAddressingMode.MULTICAST, 1);
-			GangliaReporter reporter = GangliaReporter.forRegistry(registry).convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS).build(ganglia);
+		if (config.getProperty(GANGLIA, "false").equalsIgnoreCase("true")) {
+			final GMetric ganglia = new GMetric(config.getProperty(GANGLIA_SERVERNAME),
+					Integer.parseInt(config.getProperty(GANGLIA_PORT)),
+					UDPAddressingMode.MULTICAST, 1);
+			GangliaReporter reporter = GangliaReporter.forRegistry(registry)
+					.convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS)
+					.build(ganglia);
 			reporterMap.put(GANGLIA, reporter);
 			LOG.info("Ganglia Reporter registered");
 		}
-		if (config.getBoolean(CONSOLE, false)) {
-			ConsoleReporter reporter = ConsoleReporter.forRegistry(registry).convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS).build();
+		if (config.getProperty(CONSOLE, "false").equalsIgnoreCase("true")) {
+			ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
+					.convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS)
+					.build();
 			reporterMap.put(CONSOLE, reporter);
 			LOG.info("Console reporter registered");
 		}
@@ -94,7 +100,9 @@ public class ConduitMetrics {
 	}
 
 	/**
-	 * Create a Gauge where a value can be set by using the AbsoluteGauge methods
+	 * Create a Gauge where a value can be set by using the AbsoluteGauge
+	 * methods
+	 * 
 	 * @param name
 	 * @param initalValue
 	 * @return
@@ -119,7 +127,7 @@ public class ConduitMetrics {
 	}
 
 	/*
-	 * Register a codahale.metrics type Gauge. 
+	 * Register a codahale.metrics type Gauge.
 	 */
 	@SuppressWarnings("rawtypes")
 	public static Gauge registerGauge(String name, Gauge gaugeInst) {
@@ -134,7 +142,8 @@ public class ConduitMetrics {
 	}
 
 	/**
-	 * Register a counter 
+	 * Register a counter
+	 * 
 	 * @param name
 	 * @return
 	 */
