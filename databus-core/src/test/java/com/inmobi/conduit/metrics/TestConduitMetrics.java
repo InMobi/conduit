c55d336490a82467ca175c5cbb61c7544b4faab3
samar <samar.kumar@inmobi.com>
2013-10-22 12:35:19 +0530

    tests refined

--- databus-core/src/test/java/com/inmobi/conduit/metrics/TestConduitMetrics.java
+++ databus-core/src/test/java/com/inmobi/conduit/metrics/TestConduitMetrics.java
@@ -1,6 +1,8 @@
 package com.inmobi.conduit.metrics;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.testng.Assert;
@@ -22,36 +24,52 @@ public class TestConduitMetrics {
 	
 	@Test
 	public void testRegisterAbsoluteGauge() {
-		AbsoluteGauge abGuage = ConduitMetrics.registerAbsoluteGauge("test.abs.guage", 5);
-		Assert.assertNotNull(abGuage);
-		abGuage = ConduitMetrics.registerAbsoluteGauge("test.abs.guage", 5);
-		Assert.assertNull(abGuage);
+		final String abGaugeName = "test.abs.guage";
+		AbsoluteGauge abGauge = ConduitMetrics.registerAbsoluteGauge(abGaugeName, 5);
+		Assert.assertNotNull(abGauge);
+		Assert.assertEquals(abGauge.getValue(), 5);
+		abGauge.setValue(2);
+		Assert.assertEquals(abGauge.getValue(), 2);
+		abGauge = ConduitMetrics.registerAbsoluteGauge(abGaugeName, 5);
+		Assert.assertNull(abGauge);
 
 	}
 
 	@Test
 	public void testRegisterCounter() {
-		Counter abGuage = ConduitMetrics.registerCounter("test.counter");
-		Assert.assertNotNull(abGuage);
-		abGuage = ConduitMetrics.registerCounter("test.counter");
-		Assert.assertNull(abGuage);
-		abGuage = ConduitMetrics.getCounter("test.counter");
-		Assert.assertNotNull(abGuage);
+		final String counterName = "test.counter";
+		Counter abCounter = ConduitMetrics.registerCounter(counterName);
+		Assert.assertNotNull(abCounter);
+		abCounter = ConduitMetrics.getCounter(counterName);
+		Assert.assertNotNull(abCounter);
+		abCounter.inc();
+		Assert.assertEquals(abCounter.getCount(), 1);
+		abCounter.inc(4);
+		Assert.assertEquals(abCounter.getCount(), 5);
+		// trying to register again
+		abCounter = ConduitMetrics.registerCounter(counterName);
+		Assert.assertNull(abCounter);
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testRegisterGauge() {
-		Gauge g = new Gauge<Long>() {
+		final String gaugeName = "test.guage";
+		final List<Integer> l = new ArrayList<Integer>();
+		Gauge<Integer> gaugeInst = new Gauge<Integer>() {
 			@Override
-			public Long getValue() {
-				return 1l;
+			public Integer getValue() {
+				return l.size();
 			}
 		};
-		g = ConduitMetrics.registerGauge("test.guage", g);
-		Assert.assertNotNull(g);
-		g = ConduitMetrics.registerGauge("test.guage", g);
-		Assert.assertNull(g);
+		gaugeInst = ConduitMetrics.registerGauge(gaugeName, gaugeInst);
+		Assert.assertNotNull(gaugeInst);
+		Assert.assertEquals(gaugeInst.getValue(), new Integer(0));
+		l.add(2);
+		Assert.assertEquals(gaugeInst.getValue(), new Integer(1));
+		gaugeInst = ConduitMetrics.registerGauge(gaugeName, gaugeInst);
+		Assert.assertNull(gaugeInst);
 
 	}
 
