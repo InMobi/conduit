6bbdcc97b199ecaf10ba94a4e79607368f4e7d36
raju <raju.bairishetti@inmobi.com>
2013-10-28 09:34:57 +0530

    add a unit test for validating recevied map

--- databus-worker/src/test/java/com/inmobi/databus/local/LocalStreamServiceTest.java
+++ databus-worker/src/test/java/com/inmobi/databus/local/LocalStreamServiceTest.java
@@ -18,6 +18,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -26,6 +27,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -50,8 +54,11 @@ import com.inmobi.databus.FSCheckpointProvider;
 import com.inmobi.databus.SourceStream;
 import com.inmobi.databus.TestMiniClusterUtil;
 import com.inmobi.databus.local.LocalStreamService.CollectorPathFilter;
+import com.inmobi.databus.utils.FileUtil;
+import com.inmobi.messaging.Message;
 import com.inmobi.messaging.publisher.MessagePublisher;
 import com.inmobi.messaging.publisher.MessagePublisherFactory;
+import com.inmobi.messaging.util.AuditUtil;
 
 public class LocalStreamServiceTest extends TestMiniClusterUtil {
   private static Logger LOG = Logger.getLogger(LocalStreamServiceTest.class);
@@ -581,7 +588,49 @@ cluster.getCheckpointDir()),
       service.getFileSystem().delete(
           new Path(service.getCluster().getRootDir()), true);
     }
-
   }
 
+  @Test
+  public void testFileUtil() throws Exception {
+    String streamName = "test1";
+    Path rootDir = new Path("/tmp/localServiceTest/testcluster2/mergeservice");
+    Path dataDir = new Path(rootDir, "data/test1/testcluster2");
+    FileSystem fs = dataDir.getFileSystem(new Configuration());
+    fs.mkdirs(dataDir);
+    String filenameStr = new String(streamName + "-" +
+        TestLocalStreamService.getDateAsYYYYMMDDHHmm(new Date()) + "_00001");
+    Path src = new Path(dataDir, filenameStr);
+
+    LOG.debug("Creating Test Data with filename [" + filenameStr + "]");
+    FSDataOutputStream streamout = fs.create(src);
+    String content = "Creating Test data for teststream";
+    Message msg = new Message(content.getBytes());
+    long currentTimestamp = new Date().getTime();
+    AuditUtil.attachHeaders(msg, currentTimestamp);
+    byte[] encodeMsg = Base64.encodeBase64(msg.getData().array());
+    streamout.write(encodeMsg);
+    streamout.write("\n".getBytes());
+    streamout.write(encodeMsg);
+    streamout.write("\n".getBytes());
+    long nextMinuteTimeStamp = currentTimestamp + 60000;
+    // Genearate a msg with different timestamp.  Default window period is 60sec
+    AuditUtil.attachHeaders(msg, nextMinuteTimeStamp);
+    encodeMsg = Base64.encodeBase64(msg.getData().array());
+    streamout.write(encodeMsg);
+    streamout.close();
+    Map<Long, Long> received = new HashMap<Long, Long>();
+    Path target = new Path(new Path(rootDir,
+        "system/tmp/LocalStreamService_testcluster2_test1@/" +
+        "job_local_0001/attempt_local_0001_m_000000_0/"), filenameStr + ".gz");
+    FileUtil.gzip(src, target, new Configuration(), received);
+    System.out.println("RRRRRRRR received " + received + "   sssrc " + src + "  target " + target);
+    Assert.assertEquals(2, received.size());
+    // current timestamp window = currentTimestamp - (currentTimestamp % 60000)
+    Assert.assertTrue(
+        2 == received.get(currentTimestamp - (currentTimestamp % 60000)));
+    // next timestamp window = nextMinuteTimeStamp - (nextMinuteTimeStamp %60000)
+    Assert.assertTrue(
+        1 == received.get(nextMinuteTimeStamp - (nextMinuteTimeStamp %60000)));
+    fs.delete(rootDir, true);
+  }
 }
