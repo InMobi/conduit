55649d54ca4259c0c4ff243a97a3836d595accfa
Annapurna <annapurna.vmss@inmobi.com>
2013-12-03 18:03:48 +0530

    load guava jar with virtualWebAppLoader

--- databus-visualization/conf/setenv.sh
+++ databus-visualization/conf/setenv.sh
@@ -1,4 +1,16 @@
-#! /bin/sh
+#!/bin/sh
 
-#hadoop-home-path is path till hadoop core jar
-export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=<path to hadoop core jar>
+#hadoop home path
+HADOOP_HOME=/usr/lib/hadoop-all
+for f in $HADOOP_HOME/lib/*.jar;
+do
+  if echo "$f" | grep 'guava';
+  then
+    jar=$f
+  fi
+done
+echo "Using HADOOP_HOME: $HADOOP_HOME"
+GUAVA_HOME=$jar
+echo "Using GUAVA_HOME: $GUAVA_HOME"
+export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=$HADOOP_HOME -Dguava.home=$GUAVA_HOME"
+
