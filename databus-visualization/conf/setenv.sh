3a04bc50a65d324a70e8f5f318243ef97a6365a0
Annapurna <annapurna.vmss@inmobi.com>
2013-12-04 12:45:15 +0530

    remove checking for multiple version guava jars

--- databus-visualization/conf/setenv.sh
+++ databus-visualization/conf/setenv.sh
@@ -2,11 +2,6 @@
 
 #hadoop home path
 HADOOP_HOME=<path to hadoop jars>
-count=$(ls $HADOOP_HOME/lib/guava-[0-9]*jar | wc -l )
-if test $count -gt 1
-then
-   echo "More than one guava jar available"
-   exit
-fi
+echo "Using HADOOP_HOME :	$HADOOP_HOME"
 export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=$HADOOP_HOME -Dguava.home=`ls $HADOOP_HOME/lib/guava-[0-9]*jar`"
 
