de19c166904e470576cf7fd057db6c432299c68c
Annapurna <annapurna.vmss@inmobi.com>
2013-12-04 12:16:33 +0530

    change setenv.sh to add guava jar and exit if more than 1 guava-[version] jars are present in lib

--- databus-visualization/conf/setenv.sh
+++ databus-visualization/conf/setenv.sh
@@ -1,16 +1,12 @@
-#! /bin/sh
+#!/bin/sh
 
 #hadoop home path
-HADOOP_HOME=<path to hadoop>
-for f in $HADOOP_HOME/lib/*.jar;
-do
-  if echo "$f" | grep 'guava';
-  then
-    jar=$f
-  fi
-done
-echo "Using HADOOP_HOME: $HADOOP_HOME"
-GUAVA_HOME=$jar
-echo "Using GUAVA_HOME: $GUAVA_HOME"
-export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=$HADOOP_HOME -Dguava.home=$GUAVA_HOME"
+HADOOP_HOME=/usr/lib/hadoop-all
+count=$(ls $HADOOP_HOME/lib/guava-[0-9]*jar | wc -l )
+if test $count -gt 1
+then
+   echo "More than one guava jar available"
+   exit
+fi
+export JAVA_OPTS="$JAVA_OPTS -Dhadoop.home=$HADOOP_HOME -Dguava.home=`ls $HADOOP_HOME/lib/guava-[0-9]*jar`"
 
