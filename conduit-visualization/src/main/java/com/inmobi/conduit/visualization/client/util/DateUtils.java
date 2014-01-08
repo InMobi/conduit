d6c06ba9dbc4a6c4d9e56f50bf9c28c98627671b
Annapurna <annapurna.vmss@inmobi.com>
2014-01-08 15:38:18 +0530

    [DBUS-81] Fix minute selection for previous months

--- conduit-visualization/src/main/java/com/inmobi/conduit/visualization/client/util/DateUtils.java
+++ conduit-visualization/src/main/java/com/inmobi/conduit/visualization/client/util/DateUtils.java
@@ -118,12 +118,14 @@ public class DateUtils {
     return checkSelectedDateRolledUp(date, rolledUpTillDayas);
   }
 
-  public static boolean checkSelectedDateRolledUp(Date selectedDate, int rolledUpTillDayas) {
-    String selectedDateStr = BASE_DATE_FORMATTER.format(selectedDate);
-    Date currentDate = new Date();
-    Date rolledUpDate = getDateByOffset(currentDate, -rolledUpTillDayas);
-    String rolledUpDateStr = BASE_DATE_FORMATTER.format(rolledUpDate);
-    if (selectedDateStr.compareTo(rolledUpDateStr) >= 0) {
+  public static boolean checkSelectedDateRolledUp(Date selectedDate,
+                                                  int rolledUpTillDayas) {
+    Date finalDate = new Date(selectedDate.getTime() - (selectedDate.getTime
+        () % 86400000l));
+    Date rolledUpDate = getDateByOffset(new Date(), -rolledUpTillDayas);
+    Date finalRollupDate = new Date(rolledUpDate.getTime() - (rolledUpDate
+        .getTime() % 86400000l));
+    if (!finalDate.before(finalRollupDate)) {
       return false;
     }
     return true;
