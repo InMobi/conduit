f32826163283fcfaf393a2f543f418368b2f9b0e
raju <raju.bairishetti@inmobi.com>
2014-09-14 23:31:50 +0530

    add HCatClientUtil and HCatPartitionComparator classes

---
+++ conduit-worker/src/main/java/com/inmobi/conduit/HCatClientUtil.java
@@ -0,0 +1,64 @@
+package com.inmobi.conduit;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hive.hcatalog.api.HCatClient;
+import org.apache.hive.hcatalog.api.HCatAddPartitionDesc;
+import org.apache.hive.hcatalog.cli.SemanticAnalysis.HCatSemanticAnalyzer;
+import org.apache.hive.hcatalog.common.HCatException;
+
+public class HCatClientUtil {
+  private static final Log LOG = LogFactory.getLog(HCatClientUtil.class);
+  private String metastoreURL = null;
+  protected BlockingQueue<HCatClient> buffer;
+
+  public HCatClientUtil(String metastoreURL) {
+    this.metastoreURL = metastoreURL;
+  }
+
+  public String getMetastoreUrl() {
+    return metastoreURL; 
+  }
+
+  public void createHCatClients(int numOfHCatClients, HiveConf hcatConf)
+      throws HCatException, InterruptedException {
+    buffer = new LinkedBlockingDeque<HCatClient>(numOfHCatClients);
+    for (int i = 0; i < numOfHCatClients; i++) {
+      HCatClient hcatClient = HCatClient.create(hcatConf);
+      buffer.put(hcatClient);
+    }
+    LOG.info("Total number of hcat clients are " + buffer.size());
+  }
+
+  public HCatClient getHCatClient() throws InterruptedException {
+    if (buffer != null) {
+      return buffer.poll(60, TimeUnit.SECONDS);
+    } else {
+      return null;
+    }
+  }
+
+  public void submitBack(HCatClient hcatClient) {
+    if (buffer != null) {
+      buffer.offer(hcatClient);
+    }
+  }
+
+  public void close() {
+    Iterator<HCatClient> hcatIt = buffer.iterator();
+    while(hcatIt.hasNext()) {
+      try {
+        hcatIt.next().close();
+      } catch (HCatException e) {
+        LOG.info("Exception occured while closing HCatClient ");
+      }
+    }
+  }
+}
